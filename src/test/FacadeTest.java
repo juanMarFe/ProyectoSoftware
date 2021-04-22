1f82f3349fc8572210e0e976f93335ff010b5b7b
santos130400 <santitobar2010@hotmail.com>
2021-04-21 20:36:39 -0500

    Esquema testFacade

---
+++ src/test/FacadeTest.java
@@ -0,0 +1,129 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FacadeTest {
+
+	@Test
+	void testCrearInstaSingleton() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUsuarioFactory() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testObtenerUsuario() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAllTrabajadores() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testR_TodasLasOfertas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testR_UnicaOferta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuscarEmpresas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuscarTrabajadores() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testBuscarPsicologos() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testC_Empresa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testR_Empresa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testU_Empresa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testD_Empresa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testC_Trabajador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testR_Trabajador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testU_Trabajador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testD_Trabajador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testC_Psicologo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testR_Psicologo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testU_Psicologo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testD_Psicologo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testC_AgrupacionOferta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testC_AgrupacionEmpresa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testD_Oferta() {
+		fail("Not yet implemented");
+	}
+
+}
